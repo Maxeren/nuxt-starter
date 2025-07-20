a6aec05e71ed08aae01f45bf1230d518def28829
Morten Fink Buchhave <mferiksen@gmail.com>
2025-07-20 23:23:01 +0200

    moved frontend into folder

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,13 +2,14 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+  future: { compatibilityVersion: 4 },
   experimental: { appManifest: false },
   modules: ["nuxt-icon", "@nuxt/image"],
   compatibilityDate: "2024-12-18",
